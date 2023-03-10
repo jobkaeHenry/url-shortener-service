5f15be0fb83c5708d79dfe4c177b0cc27b3738e3
Jungu Lee <1zzangjun@gmail.com>
2023-03-11 03:37:17 +0900

    :bug: 환경변수가 추가되지 않는 오류 수정

--- client/src/lib/api/axios.ts
+++ client/src/lib/api/axios.ts
@@ -1,20 +1,14 @@
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-const BASE_URL_DEV = process.env.REACT_APP_BASE_URL_DEV;
+// const BASE_URL_DEV = process.env.REACT_APP_BASE_URL_DEV;
 
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "x-CSRFToken";
-axios.defaults.baseURL = BASE_URL_DEV;
 
-console.log(process.env.NODE_DEV)
-console.log(BASE_URL)
+// axios.defaults.baseURL = BASE_URL_DEV;
+axios.defaults.baseURL = BASE_URL;
 
-if (process.env.NODE_DEV === "production") {
-  axios.defaults.baseURL = BASE_URL;
-} else if (process.env.NODE_ENV === "development") {
-  axios.defaults.baseURL = BASE_URL_DEV;
-}
 
 /**쿠키를 싣고가야하는 요청 axios */
 export const axiosPrivate = axios.create({
