1fe3e871c8aebff7077d75547fcfdbbc5cb6bee5
Jungu Lee <1zzangjun@gmail.com>
2023-03-11 04:42:17 +0900

    :bug: clipboard 실행이 안됨을 공지

--- client/src/utils/copyToClipBoard.ts
+++ client/src/utils/copyToClipBoard.ts
@@ -1,8 +1,10 @@
 export const handleCopyClipBoard = async (text: string) => {
   try {
-    await navigator.clipboard.writeText(text);
+    await navigator.clipboard.writeText(String(text));
     alert("클립보드에 링크가 복사되었습니다.");
   } catch (e) {
-    console.log(e)
+    alert(
+      "navigator.clipboard.writeText를 사용해 구현되어있지만, 클립보드는 HTTPS에서만 접근이 가능하므로 실행되지않습니다"
+    );
   }
 };
