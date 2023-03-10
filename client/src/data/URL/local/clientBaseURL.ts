2824fe9c22ad8e1576bc4c645a75a9fa25f7e070
Jungu Lee <1zzangjun@gmail.com>
2023-03-11 04:21:27 +0900

    :art: client baseURL 수정

--- client/src/data/URL/local/clientBaseURL.ts
+++ client/src/data/URL/local/clientBaseURL.ts
@@ -1,1 +1,1 @@
-export const clientBaseURL = "http://doznshortner.s3-website.ap-northeast-2.amazonaws.com" as const;
+export const clientBaseURL = window.location.protocol + "//" + window.location.host;
